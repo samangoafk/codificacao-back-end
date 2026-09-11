70da8d569d18c280421ffd15f7b7c4a83ba3b2ef
adam castro <riuksilva72@gmail.com>
2026-09-11 16:46:30 -0300

    fix: corrige pequeno erro ortográfico nos comentários

--- aula01-revisao-nodejs-npm/diagnostico.js
+++ aula01-revisao-nodejs-npm/diagnostico.js
@@ -2,13 +2,13 @@
 const os = require('os');
 
 //! Exibindo informações sobre o sistema operacional e hardware
-console.log('=== DIAGNOSTICO DO SERVIDOS ===\n');
+console.log('=== DIAGNOSTICO DO SERVIDOR ===\n');
 
 const plataforma = os.platform(); //! obtendo a plataforma
 const memoriaTotal = (os.totalmem() / (1024 ** 3)).toFixed(2);
 const memoriaLivre = (os.freemem() / (1024 ** 3)).toFixed(2);
 const cpus = os.cpus(); //! obtendo informações sobre performance
- 
+
 //! Exibindo informaões sobre o sistema 
 console. log(`Arquitetura OS: ${plataforma}`);
 console. log(`Memória RAM Total: ${memoriaTotal} GB`);
